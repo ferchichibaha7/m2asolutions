f860720fef353a2a4ea213f823bd3fff2c58f9c6
Meschac Irung <meschacirung@gmail.com>
2022-12-01 02:27:00 +0200

    add smooth scroll

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ import "./assets/app.css";
 import "./src/js/faqs.js";
 import "./src/js/theme.js";
 import "./src/js/solutions.js";
+
+const links = document.querySelectorAll(".nav-link");
+ console.log(links)
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+ 
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
