9432261f9a2fe85f2463c3e753aba09a65015f1b
Meschac Irung <meschacirung@gmail.com>
2022-12-01 09:52:13 +0200

    fix navbar animation

--- app.js
+++ app.js
@@ -1,23 +1,5 @@
 import "./assets/app.css";
+import "./src/js/navbar.js"
 import "./src/js/faqs.js";
 import "./src/js/theme.js";
 import "./src/js/solutions.js";
-
-const links = document.querySelectorAll(".nav-link");
-const toggler = document.querySelector('#hbr')
-
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
- 
-function clickHandler(e) {
-  e.preventDefault();
-  toggler.removeAttribute('checked')
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
- 
-  scroll({
-    top: offsetTop,
-    behavior: "smooth"
-  });
-}
---
+++ src/js/navbar.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector('#hamburger')
+const navbar = document.querySelector('#navbar')
+const links = navbar.querySelectorAll(".nav-link");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+ 
+hamburger.addEventListener('click', ()=>{
+    navbar.classList.toggle('navbar-active')
+})
+
+function clickHandler(e) {
+  const href = this.getAttribute("href");
+  !href.includes('/') ? e.preventDefault() : null; 
+  const offsetTop = document.querySelector(href).offsetTop;
+  navbar.classList.remove('navbar-active')
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
