3f0d24e9d96851b2b0a237c9dce00e6dc25c7f63
Meschac Irung <meschacirung@gmail.com>
2023-04-11 19:44:14 +0200

    fix [navbar] : enable closing navbar on link click

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,6 +1,13 @@
 const hamburger = document.querySelector("#hamburger");
 const navbar = document.querySelector("#navbar");
+const links = navbar.querySelectorAll("a");
 
 hamburger.addEventListener("click", () => {
     navbar.classList.toggle("navbar-active");
+});
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        navbar.classList.toggle("navbar-active");
+    });
 });
